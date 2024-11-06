09c79042b235d10037dc980745b2392a38d546a5
Sandilya Jandhyala <jysandilya@gmail.com>
2024-11-06 13:33:13 +0000

    Add a basic sun flare around the skydome

--- Core/Shaders/skydome_ps.hlsl
+++ Core/Shaders/skydome_ps.hlsl
@@ -18,31 +18,22 @@ float4 main(VSOutput input) : SV_TARGET
     float3 L = -normalize(g_lightdirection);
     float3 V = normalize(input.texcoord);
     
-    float3 horizontalL = normalize(float3(L.x, 0, L.z));
+    float3 horizontalL = normalize(float3(L.x, lerp(L.y, 0, g_drawSunCircle), L.z));
     
     float cosTheta = dot(normalize(input.texcoord), up);
     float cosL = max(dot(L, up), 0);
     float cosHorizontalL = dot(horizontalL, V) * 0.5 + 0.5;
     
-    float3 orange = float3(0.86, 0.49, 0.06);
     float3 blue = float3(0.1, 0.14, 0.6);
     float3 black = float3(0, 0, 0);
     
     float3 skyColour;
-    float3 skyBase = lerp(orange, blue, cosL);
+    float3 skyBase = lerp(g_sunColour, blue, cosL);
     skyBase = lerp(blue, skyBase, cosHorizontalL);
-    if (cosTheta < 0)
-    {          
-        float lerpFactor = saturate(-2 * cosTheta);
-        skyColour = lerp(skyBase, black, lerpFactor);
-    }
-    else
-    {
-        skyColour = lerp(skyBase, blue, cosTheta);
-    }
-    
-    
-    
+
+    // TODO: Get the sun flare up at the height of the sun
+    skyColour = lerp(skyBase, blue, pow(abs(cosTheta), lerp(0.8, 1, g_drawSunCircle)));
+
     float LdotV = max(dot(L, V), 0);
     
     float3 sunCircle = g_drawSunCircle * (g_sunExp / 16) * g_sunColour * pow(LdotV, g_sunExp);
