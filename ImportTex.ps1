bb2a8b1d81a656c5c82dad293126437ce731dc0e
Sandilya Jandhyala <jysandilya@gmail.com>
2025-04-22 18:17:21 +0100

    Reduce the leaf texture size, which hasn't done much

--- ImportTex.ps1
+++ ImportTex.ps1
@@ -2,26 +2,35 @@
     [switch]$srgb,
     [switch]$height,
     [switch]$singleChannel,
-    [string]$filePath
+    [string]$filePath,
+    [string]$width
  )
+
+# Build texconv arguments
+$texconvArgs = @($filePath, '-o', 'Assets', '-y', '-m', '5')
+
+if (-not [string]::IsNullOrEmpty($width))
+{
+    $texconvArgs += @('-w', $width, '-h', $width)
+}
 
 if ($height)
 {
     exiftool.exe -ColorSpace=Uncalibrated -overwrite_original $filePath
-    texconv $filePath -o Assets -y -m 5 -f R16_UNORM
+    texconv $texconvArgs -f R16_UNORM
 }
 elseif ($singleChannel)
 {
     exiftool.exe -ColorSpace=Uncalibrated -overwrite_original $filePath
-    texconv $filePath -o Assets -y -m 5 -f BC4_UNORM
+    texconv $texconvArgs -f BC4_UNORM
 }
 elseif($srgb)
 {
     exiftool.exe -ColorSpace=sRGB -overwrite_original $filePath
-    texconv $filePath -o Assets -y -m 5 -f BC7_UNORM_SRGB
+    texconv $texconvArgs -f BC7_UNORM_SRGB
 }
 else
 {
     exiftool.exe -ColorSpace=Uncalibrated -overwrite_original $filePath
-    texconv $filePath -o Assets -y -m 5 -f BC7_UNORM
+    texconv $texconvArgs -f BC7_UNORM
 }
